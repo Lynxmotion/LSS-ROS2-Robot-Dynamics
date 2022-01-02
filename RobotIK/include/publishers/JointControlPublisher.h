854046c7ac302781c56b70c2641f8a5fa2c49d4b
Colin MacKenzie <colin@flyingeinstein.com>
2022-01-02 17:48:35 -0500

    added JointControlPublisher for publishing joint control data to ros2_controls (or whatever)

---
+++ RobotIK/include/publishers/JointControlPublisher.h
@@ -0,0 +1,89 @@
+//
+// Created by guru on 1/2/22.
+//
+
+#ifndef ROBOT_DYNAMICS_JOINTCONTROLPUBLISHER_H
+#define ROBOT_DYNAMICS_JOINTCONTROLPUBLISHER_H
+
+#include <rclcpp/node.hpp>
+#include <rclcpp_lifecycle/lifecycle_node.hpp>
+#include "lifecycle_msgs/srv/get_state.hpp"
+#include "lifecycle_msgs/srv/change_state.hpp"
+
+#include <trajectory_msgs/msg/joint_trajectory.hpp>
+#include <trajectory_msgs/msg/joint_trajectory_point.hpp>
+#include <std_msgs/msg/float64_multi_array.hpp>
+
+#include <state.h>
+#include <indexes.h>
+
+namespace robotik {
+
+    class JointControlPublisher {
+    public:
+        using SharedPtr = std::shared_ptr<JointControlPublisher>;
+        using CallbackReturn = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
+
+        JointControlPublisher(
+                rclcpp_lifecycle::LifecycleNode& node,
+                std::string position_controller,
+                std::string effort_controller,
+                std::string frame_id = "");
+        //JointControlPublisher(rclcpp::Node& node, std::string topic_name = "joint_states", std::string frame_id = "");
+
+        CallbackReturn on_activate();
+
+        CallbackReturn on_deactivate();
+
+        void state(const std::shared_ptr<JointState>& state);
+
+        ///@brief Set the joints to be published to the joint controllers (i.e. ros2_controls)
+        /// This will select the subset of joints from the JointState to publish. We don't typically send fixed or
+        /// non-transmission joints to the joint controllers.
+        void set_joints(const std::vector<std::string>& joint_names);
+
+        [[nodiscard]] inline const std::shared_ptr<JointState>& state() const { return state_; }
+        [[nodiscard]] inline std::shared_ptr<JointState>& state() { return state_; }
+
+        ///@brief Publish all joint control messages
+        /// Joint state/angles are read from the target state and sent to the joint position/velocity controllers.
+        void publish();
+
+    protected:
+        std::string frame_id_;
+        std::string position_controller_name, effort_controller_name;
+        std::shared_ptr<JointState> state_;
+
+        bool efforts_updated;
+
+        // lookup the joint index in state using the index
+        JointStateOrdinalMap joint_ordinal_map;
+
+        rclcpp::Logger logger;
+
+        // trajectory publisher
+        trajectory_msgs::msg::JointTrajectory::SharedPtr joint_trajectory_msg_;
+        rclcpp_lifecycle::LifecyclePublisher<trajectory_msgs::msg::JointTrajectory>::SharedPtr joint_trajectory_pub_;
+
+        // effort publisher
+        std_msgs::msg::Float64MultiArray::SharedPtr joint_efforts_msg_;
+        rclcpp_lifecycle::LifecyclePublisher<std_msgs::msg::Float64MultiArray>::SharedPtr joint_efforts_pub_;
+
+        // ability to change the lifecycle of the Ros2 controllers
+        lifecycle_msgs::srv::ChangeState::Request::SharedPtr jointctrl_change_state_request_;
+        lifecycle_msgs::srv::GetState::Request::SharedPtr jointctrl_get_state_request_;
+        rclcpp::Client<lifecycle_msgs::srv::GetState>::SharedPtr jointctrl_get_state_client_;
+        rclcpp::Client<lifecycle_msgs::srv::ChangeState>::SharedPtr jointctrl_change_state_client_;
+        rclcpp::Client<lifecycle_msgs::srv::GetState>::SharedFuture jointctrl_get_state_future_;
+        rclcpp::Client<lifecycle_msgs::srv::ChangeState>::SharedFuture jointctrl_change_state_future_;
+
+        void publish_joint_trajectory();
+        void publish_efforts();
+
+        // todo: refactor this code into a LifecycleNodeService class for getting and setting service state
+        //     also add a ControllerManager one
+        void activate_position_controller(bool active);
+    };
+
+} // ns:robotik
+#endif //ROBOT_DYNAMICS_JOINTCONTROLPUBLISHER_H
---
+++ RobotIK/src/publishers/JointControlPublisher.cpp
@@ -0,0 +1,173 @@
+//
+// Created by guru on 1/2/22.
+//
+
+
+#include "publishers/JointControlPublisher.h"
+
+#include <utility>
+//#include "types.h"
+
+namespace robotik {
+
+
+JointControlPublisher::JointControlPublisher(
+        rclcpp_lifecycle::LifecycleNode& node,
+        std::string position_controller,
+        std::string effort_controller,
+        std::string frame_id)
+    : frame_id_(std::move(frame_id)),
+    position_controller_name(std::move(position_controller)),
+    effort_controller_name(std::move(effort_controller)),
+    efforts_updated(false),
+    logger(node.get_logger())
+{
+    // publisher for joint trajectory
+    joint_trajectory_pub_ = node.create_publisher<trajectory_msgs::msg::JointTrajectory>(
+            "/"+position_controller_name+"/joint_trajectory",
+            10);
+
+    // create a joint publisher, we'll control the servo output
+    if(!joint_trajectory_msg_)
+        joint_trajectory_msg_ = std::make_shared<trajectory_msgs::msg::JointTrajectory>();
+
+    // effort control
+    if(!effort_controller_name.empty()) {
+        joint_efforts_pub_ = node.create_publisher<std_msgs::msg::Float64MultiArray>(
+                effort_controller_name,
+                10);
+        if(!joint_efforts_msg_) {
+            joint_efforts_msg_ = std::make_shared<std_msgs::msg::Float64MultiArray>();
+            if(!joint_trajectory_msg_->joint_names.empty())
+                joint_efforts_msg_->data.resize(joint_trajectory_msg_->joint_names.size());
+        }
+    } else {
+        RCLCPP_WARN(logger, "No effort_controller parameter specified so effort control will be disabled");
+    }
+
+    // ability to control state of the joint controller
+    // tutorial: https://github.com/ros2/demos/blob/master/lifecycle/src/lifecycle_service_client.cpp
+    jointctrl_change_state_request_ = std::make_shared<lifecycle_msgs::srv::ChangeState::Request>();
+    jointctrl_get_state_client_ = node.create_client<lifecycle_msgs::srv::GetState>(
+            position_controller_name + "/get_state",
+            rmw_qos_profile_services_default,
+            nullptr);
+    jointctrl_change_state_client_ = node.create_client<lifecycle_msgs::srv::ChangeState>(
+            position_controller_name + "/change_state",
+            rmw_qos_profile_services_default,
+            nullptr);
+}
+
+void JointControlPublisher::state(const std::shared_ptr<JointState>& state)
+{
+    if(state)
+        state_ = state;
+    else
+        state_.reset();
+}
+
+void JointControlPublisher::set_joints(const std::vector<std::string> &joint_names)
+{
+    // new joint names for publishing
+    size_t N = joint_names.size();
+    if(!joint_trajectory_msg_)
+        joint_trajectory_msg_ = std::make_shared<trajectory_msgs::msg::JointTrajectory>();
+    joint_trajectory_msg_->joint_names = joint_names;
+
+    if(!joint_efforts_msg_)
+        joint_efforts_msg_ = std::make_shared<std_msgs::msg::Float64MultiArray>();
+    joint_efforts_msg_->data.resize(N);
+
+    joint_ordinal_map.resize(N);
+}
+
+JointControlPublisher::CallbackReturn JointControlPublisher::on_activate()
+{
+    if(joint_trajectory_pub_)
+        joint_trajectory_pub_->on_activate();
+    if(joint_efforts_pub_)
+        joint_efforts_pub_->on_activate();
+    return CallbackReturn::SUCCESS;
+}
+
+JointControlPublisher::CallbackReturn JointControlPublisher::on_deactivate()
+{
+    if(joint_trajectory_pub_)
+        joint_trajectory_pub_->on_deactivate();
+    if(joint_efforts_pub_)
+        joint_efforts_pub_->on_deactivate();
+    return CallbackReturn::SUCCESS;
+}
+
+void JointControlPublisher::activate_position_controller(bool active)
+{
+    if(active)  {
+        jointctrl_change_state_request_->transition.id = lifecycle_msgs::msg::Transition::TRANSITION_ACTIVATE;
+        jointctrl_change_state_future_ = jointctrl_change_state_client_->async_send_request(jointctrl_change_state_request_);
+    } else {
+        jointctrl_change_state_request_->transition.id = lifecycle_msgs::msg::Transition::TRANSITION_DEACTIVATE;
+        jointctrl_change_state_future_ = jointctrl_change_state_client_->async_send_request(jointctrl_change_state_request_);
+    }
+}
+
+void JointControlPublisher::publish_joint_trajectory()
+{
+    if(!state_)
+        return;
+
+    if (state_->joints.size() != (size_t) state_->position.rows()) {
+        throw std::runtime_error(
+                    "refuse to publish when joint names and positions arrays are not equal length");
+    }
+
+    // todo: publishing joints order should match Q_NR joint order
+    if (joint_trajectory_msg_->joint_names.empty()) {
+        set_joints(state_->joints);
+    }
+
+    auto nj = joint_trajectory_msg_->joint_names.size();
+
+    // allocate a single trajectory keyframe
+    if (joint_trajectory_msg_->points.size() != 1) {
+        joint_trajectory_msg_->points.resize(1);
+    }
+
+    // set the trajectory
+    auto &points = joint_trajectory_msg_->points[0];
+    if (points.positions.size() != nj)
+        points.positions.resize(nj);
+
+    for (size_t j = 0, _j = nj; j != _j; j++) {
+        // lookup joint position using index
+        auto j_name = joint_trajectory_msg_->joint_names[j];
+        auto j_idx = joint_ordinal_map.resolve(*state_, j, j_name);
+
+        if(j_idx < 0) {
+            // joint name was not found, this is a trajic error
+            throw std::runtime_error(
+                    "cannot publish joint " + j_name + " because it is not found in state");
+        }
+
+        // add the joint position
+        points.positions[j] = state_->position(j_idx);
+        // todo: publish velocity and acceleration for trajectories
+    }
+
+    // publish new control values
+    joint_trajectory_pub_->publish(*joint_trajectory_msg_);
+}
+
+void JointControlPublisher::publish_efforts() {
+    if(joint_efforts_pub_) {
+        joint_efforts_pub_->publish(*joint_efforts_msg_);
+        efforts_updated = false;
+    }
+}
+
+void JointControlPublisher::publish() {
+    publish_joint_trajectory();
+    if(efforts_updated)
+        publish_efforts();
+}
+
+}
