2a2ca44f161ebb113b11952b8be6241d28611f8d
Colin MacKenzie <colin@flyingeinstein.com>
2022-01-02 23:58:35 -0500

    removed no longer used types from types.h

--- RobotIK/include/types.h
+++ RobotIK/include/types.h
@@ -16,17 +16,6 @@ namespace robotik {
 
 extern const std::string DEFAULT_COMPONENT_TOPIC_PREFIX;
 
-using TwoCC = uint16_t;
-using FourCC = uint32_t;
-
-constexpr TwoCC operator"" _2cc(const char id[2], std::size_t  len) {
-    return (len==1)
-           ? id[0]
-           : (id[1] << 8) + id[0];
-}
-
-const TwoCC AnyCC = 0;
-
 constexpr unsigned long bit(unsigned short n) { return (((unsigned long)1)<<n); }
 
 constexpr unsigned long operator "" _bit(unsigned long long n) {
@@ -50,83 +39,6 @@ const ParameterMask IMU                     = 11_bit;
 const ParameterMask DEFAULT_PARAMETERS = POSITION | VELOCITY;
 const ParameterMask DEFAULT_VISUALS = TF | POSITION | CENTER_OF_MASS | CENTER_OF_PRESSURE /*| INTERNAL_FORCE | EXTERNAL_FORCE*/ | CONTACTS | SUPPORT_POLYGON;
 
-
-using Names = std::vector<std::string>;
-using Ordinals = std::vector<size_t>;
-using Frames = std::vector<KDL::Frame>;
-using SegmentTransforms = std::map<std::string, KDL::Frame>;
-
-
-class JointData {
-public:
-    double position;
-    double velocity;
-
-    inline JointData() : position(0), velocity(0) {}
-
-    inline void set(double _position, double _velocity) {
-        position = _position;
-        velocity = _velocity;
-    }
-};
-
-class NamedJointData : public JointData {
-public:
-    std::string name;
-    KDL::Frame tf;
-};
-
-using NamedJointArray = std::vector<NamedJointData>;
-using JointMap = std::map<std::string, JointData>;
-
-
-
-class Origin {
-public:
-    using Type = enum {
-        User,
-        Interpolator,
-        Control,
-        Behavior
-    };
-
-    TwoCC id;
-    Type type;
-
-    Origin(Type _type, TwoCC _id = AnyCC) : id(_id), type(_type) {}
-
-    static const Origin None;
-};
-
-class ElementKey {
-public:
-    Origin origin;               // is this worth it?
-    TwoCC symbol;                // symbolic type ID
-    std::string name;            // arbitrary name or segment name
-
-    ElementKey()
-            : origin(Origin::None), symbol(0)
-    {}
-    ElementKey(TwoCC _symbol)
-            : origin(Origin::None), symbol(_symbol)
-    {}
-    ElementKey(TwoCC _symbol, std::string _name)
-            : origin(Origin::None), symbol(_symbol), name(_name)
-    {}
-    ElementKey(TwoCC _symbol, const char* _name)
-            : origin(Origin::None), symbol(_symbol), name(_name)
-    {}
-
-    std::size_t hash() const;
-
-    std::string toString() const;
-
-    inline bool operator==(const ElementKey& rhs) const { return symbol==rhs.symbol && name==rhs.name; }
-    inline bool operator!=(const ElementKey& rhs) const { return symbol!=rhs.symbol || name!=rhs.name; }
-
-    inline bool operator<(const ElementKey& rhs) const { return symbol<rhs.symbol || (symbol == rhs.symbol && name<rhs.name); }
-};
-
 class EulerRotation {
 public:
     double roll, pitch, yaw;
@@ -135,16 +47,5 @@ public:
 };
 
 } // ns::robotik
-
-
-// custom specialization of std::hash for elements
-// injected into std name space
-namespace std {
-    template<> struct hash<robotik::ElementKey> {
-        inline std::size_t operator()(robotik::ElementKey const& s) const noexcept {
-            return s.hash();
-        }
-    };
-}
 
 #endif //LSS_HUMANOID_TYPES_H
--- RobotIK/src/types.cpp
+++ RobotIK/src/types.cpp
@@ -8,25 +8,5 @@ namespace robotik {
 
 const std::string DEFAULT_COMPONENT_TOPIC_PREFIX = "robot_dynamics";
 
-std::size_t ElementKey::hash() const {
-    std::size_t h1 = std::hash<uint16_t>{}(symbol);
-    std::size_t h2 = std::hash<std::string>{}(name);
-    return h1 ^ (h2 << 1U); // or use boost::hash_combine (see Discussion)
-}
-
-std::string ElementKey::toString() const {
-    std::string s;
-    uint16_t sym = symbol;
-    char a = sym & 0x7f;
-    char b = (sym >> 8U) & 0x7f;
-    if(isprint(a))
-        s += a;
-    if(isprint(b))
-        s += b;
-    s += ':';
-    s += name;
-    return s;
-}
-
 } // ns:robotik
 
